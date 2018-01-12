5299fcc974d4a42a4aa5d94b8c14571a95ea1c2b
tangkeyu <tangkeyu@vip.163.com>
2018-01-12 22:35:14 +0800

    add dist folder

---
+++ dist/index.js
@@ -0,0 +1,131 @@
+import Maker from './maker';
+import * as _ from 'lodash';
+// import {VueC} from "vue/types/vue";
+import Vue from "vue";
+var PreLoader = /** @class */ (function () {
+    /**
+     * 私有构造
+     * @param {object} config
+     */
+    function PreLoader(config) {
+        if (config === void 0) { config = {}; }
+        /**
+         * 需加载的组件信息
+         * @type {Map<string, Object>}
+         */
+        this.components = new Map();
+        if (_.has(config, 'callback')) {
+            this.setCallback(_.get(config, 'callback'));
+        }
+        this.maker = new Maker(config);
+        this.maker.make();
+        var makerConfig = this.maker.getConfig();
+        if (_.has(makerConfig, 'cacheFile')) {
+            this.loadMap(require('.' + _.get(makerConfig, 'cacheFile')));
+        }
+    }
+    /**
+     * 获得单例实例
+     * @param {object} config
+     * @returns {PreLoader}
+     */
+    PreLoader.getInstance = function (config) {
+        if (config === void 0) { config = {}; }
+        if (!PreLoader.instance) {
+            PreLoader.instance = new PreLoader(config);
+        }
+        return PreLoader.instance;
+    };
+    /**
+     * Vue plugins 的安装方法
+     * @param Vue
+     * @param {object} options
+     */
+    PreLoader.install = function (Vue, options) {
+        if (options === void 0) { options = {}; }
+        var loader = PreLoader.getInstance(options);
+        if (_.has(options, 'callback')) {
+            loader.setCallback(options.callback);
+        }
+        Vue.prototype.$loader = loader;
+    };
+    /**
+     * 加载一个新的组件列表
+     * @param {object} componentMap
+     */
+    PreLoader.prototype.loadMap = function (componentMap) {
+        var _this = this;
+        _.forEach(componentMap, function (com, name) {
+            if (_.isString(name) && _.isFunction(com)) {
+                _this.components.set(name, {
+                    loader: com,
+                    loaded: false,
+                    name: name
+                });
+            }
+        });
+    };
+    /**
+     * 返回 Maker
+     * @returns {Maker}
+     */
+    PreLoader.prototype.getMaker = function () {
+        return this.maker;
+    };
+    /**
+     * 设置加载完成后的callback
+     * @param {Function} callback
+     */
+    PreLoader.prototype.setCallback = function (callback) {
+        this.loadedCallback = callback;
+    };
+    /**
+     * 获取一个模块的加载函数
+     * @param {string} name
+     * @returns {any}
+     */
+    PreLoader.prototype.get = function (name) {
+        var self = this, callback = this.loadedCallback;
+        if (this.components && this.components.has(name)) {
+            var item = this.components.get(name);
+            if (!_.has(item, 'loader')) {
+                console.error('module should have \"loader\" property');
+            }
+            var loaderFunction_1 = _.get(item, 'loader');
+            if (_.isFunction(callback)) {
+                return function () { return loaderFunction_1().then(function (com) {
+                    callback(name);
+                    return com;
+                }); };
+            }
+            else {
+                return function () { return loaderFunction_1(); };
+            }
+        }
+        console.error('component not found: ' + name);
+    };
+    /**
+     * 将组件用vue.component方法注册
+     * @param {string} name
+     */
+    PreLoader.prototype.register = function (name) {
+        try {
+            var com = this.get(name);
+            Vue.component(name, com);
+        }
+        catch (e) {
+            console.error('module "' + name + '" cannot register');
+        }
+    };
+    /**
+     * 注册所有组件
+     */
+    PreLoader.prototype.registerAll = function () {
+        for (var name_1 in this.components) {
+            this.register(name_1);
+        }
+    };
+    return PreLoader;
+}());
+export default PreLoader;
+//# sourceMappingURL=index.js.map
---
+++ dist/maker.js
@@ -0,0 +1,148 @@
+import * as _ from 'lodash';
+import * as fsExtra from 'fs-extra';
+import * as path from 'path';
+var Maker = /** @class */ (function () {
+    /**
+     * 构造器
+     * @param {object} config
+     */
+    function Maker(config) {
+        if (config === void 0) { config = {}; }
+        /**
+         * 配置文件
+         * @type {{path: {root: string; except: any[]}; files: {test: RegExp; except: any[]}[]; cacheFile: string}}
+         */
+        this.config = {
+            path: {
+                root: './src',
+                exclude: []
+            },
+            files: [{
+                    test: /\.vue$/,
+                    exclude: []
+                }],
+            cacheFile: './preloader.mapping.js'
+        };
+        /**
+         * 查找到的文件列表
+         */
+        this.files = new Map();
+        this.config = _.defaultsDeep(config, this.config);
+    }
+    /**
+     * 生成mapping文件
+     */
+    Maker.prototype.make = function () {
+        this.scanDir(this.config.path.root);
+        this.output();
+    };
+    /**
+     * 返回config
+     * @returns {object}
+     */
+    Maker.prototype.getConfig = function () {
+        return this.config;
+    };
+    /**
+     * 扫描文件夹
+     * @param {string} path
+     */
+    Maker.prototype.scanDir = function (path) {
+        var files = fsExtra.readdirSync(path), self = this;
+        for (var _i = 0, files_1 = files; _i < files_1.length; _i++) {
+            var filename = files_1[_i];
+            var currentPath = path + '/' + filename;
+            if (fsExtra.pathExistsSync(currentPath)) {
+                var stat = fsExtra.statSync(currentPath);
+                //如果是目录，则继续递归
+                if (stat.isDirectory() && !this.isExcludeDir(filename)) {
+                    this.scanDir(currentPath);
+                }
+                else if (this.isTargetFile(filename)) {
+                    this.parseFile(currentPath);
+                }
+            }
+        }
+        ;
+    };
+    /**
+     * 判断文件是否是需要排除的文件
+     * @param {string} name
+     * @returns {boolean}
+     */
+    Maker.prototype.isExcludeDir = function (name) {
+        return this.isHit(name, this.config.path.exclude);
+    };
+    /**
+     * 判断是否是目标文件
+     * @param {string} name
+     * @returns {boolean}
+     */
+    Maker.prototype.isTargetFile = function (name) {
+        return this.isHit(name, this.config.files);
+    };
+    /**
+     * name是否被conditions的条件命中
+     * conditions是一个条件数组，可以包含string和regexp
+     * string按包含计算命中
+     * regexp按匹配计算命中
+     * @param {string} name
+     * @param {any[]} conditions
+     * @returns {boolean}
+     */
+    Maker.prototype.isHit = function (name, conditions) {
+        if (conditions.length) {
+            for (var _i = 0, conditions_1 = conditions; _i < conditions_1.length; _i++) {
+                var exp = conditions_1[_i];
+                if (_.isRegExp(exp) && exp.test(name)) {
+                    return true;
+                }
+                else if (_.isString(exp) && name.indexOf(exp) !== -1) {
+                    return true;
+                }
+                else if (_.isObject(exp)) {
+                    if (
+                    //如果有正则，则对name判断
+                    (_.has(exp, 'test') && _.isRegExp(exp.test) && exp.test.test(name))
+                        //如果有except，则对name进行排除
+                        && (_.has(exp, 'exclude') && !this.isHit(name, exp.exclude))) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    };
+    /**
+     * 解析文件
+     * @param {string} file
+     */
+    Maker.prototype.parseFile = function (file) {
+        var name = path.basename(file, path.extname(file));
+        if (name) {
+            this.files.set(name, file);
+        }
+    };
+    /**
+     * 输出
+     */
+    Maker.prototype.output = function () {
+        var template = "let PreLoaderMapping = { \n", targetFile = this.config.cacheFile, requireFun = 'import';
+        //测试环境
+        if (process.env.NODE_ENV == 'test') {
+            requireFun = 'require';
+            this.config.cacheFile = targetFile = './preloader.mapping.ts';
+        }
+        this.files.forEach(function (path, name) {
+            template += "\"" + name + "\": () => " + requireFun + "(/* webpackChunkName: \"" + name + "\" */ \"" + path + "\"), \n";
+        });
+        template += "};\n";
+        template += "export default PreLoaderMapping; \n";
+        template += "module.exports = PreLoaderMapping; \n";
+        // fsExtra.ensureFile(targetFile);
+        fsExtra.writeFileSync(targetFile, template, 'utf-8');
+    };
+    return Maker;
+}());
+export default Maker;
+//# sourceMappingURL=maker.js.map
